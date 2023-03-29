79f733a2322ae6b2eb481f7961ba40a012d280ac
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-03-29 14:16:55 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import { decrement, increment } from './counterSlice';
 import { RootState } from './store';
 import {useEffect} from "react";
 
+let initApp = false
+
 function App() {
   const count = useSelector((state: RootState) => state.counter.value)
   const dispatch = useDispatch()
 
   useEffect(() => {
+    if (initApp) return
+    initApp = true
+
     function* getNumber(): Iterator<number> {
-      return 5;
+      yield 1;
+      yield 2;
+      yield 3;
     }
     const probablyIterator = getNumber()
     console.log(probablyIterator.next())
+    console.log(probablyIterator.next())
+    console.log(probablyIterator.next())
+    console.log(probablyIterator.next())
 
-    //
   }, [])
 
   return (
